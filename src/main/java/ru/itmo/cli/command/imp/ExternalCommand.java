ce839a12517128fce52d5bb1a8d631173a0bfb4a
Vlasdislav <v.shchetinin.v@gmail.com>
2024-02-25 19:23:07 +0300

    chore: initial pwd, echo, state, external

--- src/main/java/ru/itmo/cli/command/imp/ExternalCommand.java
+++ src/main/java/ru/itmo/cli/command/imp/ExternalCommand.java
@@ -3,13 +3,21 @@ package ru.itmo.cli.command.imp;
 import ru.itmo.cli.command.BaseCommand;
 import ru.itmo.cli.console.AppState;
 
-public class ExternalCommand extends BaseCommand {
-    public ExternalCommand(String[] args, AppState state) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class ExternalCommand extends BaseCommand {
+    private final String[] args;
+    private final AppState state;
+    public ExternalCommand(String[] args, AppState state) {
+        this.args = args;
+        this.state = state;
     }
 
     @Override
     public void execute() {
-
+        System.out.println("Error: Unknown command!");
     }
 }
--- src/main/java/ru/itmo/cli/command/imp/PwdCommand.java
+++ src/main/java/ru/itmo/cli/command/imp/PwdCommand.java
@@ -1,14 +1,18 @@
 package ru.itmo.cli.command.imp;
 
+import ru.itmo.cli.App;
 import ru.itmo.cli.command.BaseCommand;
 import ru.itmo.cli.console.AppState;
 
 public class PwdCommand extends BaseCommand {
+    private final String[] args;
+    private final AppState state;
     public PwdCommand(String[] args, AppState state) {
-
+        this.args = args;
+        this.state = state;
     }
     @Override
     public void execute() {
-
+        System.out.println(state.getPath());
     }
 }
